f65a4049f83911975202fc9215aca969c92b9424
Salehin Rahman Khan <khan1fm@uwindsor.ca>
2020-12-20 21:23:06 -0500

    Django Polls Project
    Testing purpose Django Webapp development

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     #127.0.0.1/polls/
+    url(r'^(?P<question_id>[0-9]+)/detail$', views.detail, name="detail"),
+    #127.0.0.1/polls/0-9
+    url(r'^(?P<question_id>[0-9]+)/results$', views.results, name="results"),
+    url(r'^(?P<question_id>[0-9]+)/vote$', views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Question, choices
 # Create your views here.
+from django.template import loader, RequestContext
+
+
+def index(request):
+    latest_questions = Question.objects.order_by('pub_date')[:5]  # five is to have the first five questions
+    # output = ", ".join(q.question_text for q in latest_questions)
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_questions': latest_questions
+    })
+    return HttpResponse(template.render(context))
+
+
+def detail(request, question_id):
+    return HttpResponse("THE QUESTION IS %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("The results %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("The Votes %s" % question_id)
